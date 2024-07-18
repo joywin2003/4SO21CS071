4c9a7c4bbc2b76f03e38e3b96c480575b2bae41d
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-07-18 20:05:32 +0530

    bug: the bearer token expries within 5 minutes and required refetch every time

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
 dotenv.config();
 
-const app = express()
-
+const app = express();
 
 const port = process.env.PORT || 3000;
 const token = process.env.TOKEN;
+const baseUrl = process.env.BASE_URL;
 
+app.get("/", (req, res) => {
+  console.log(port, token, baseUrl);
+  res.send("I am a backend server");
+});
 
+app.get("/api", async (req, res) => {
+  try {
+    console.log(`${baseUrl}/test/even`);
+    const response = await axios(`${baseUrl}even`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data);
 
-
-app.get('/', (req, res) => {
-    console.log(port, token, baseUrl)
-  res.send("I am a backend server")
-})
+    // res.send(response.data);
+  } catch (error) {
+    console.log("error",error);
+  }
+  
+});
 
 app.listen(port, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
