6ac77fff0c0ffdd8b8275dc5d5e6c3f4a6a49b09
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-07-18 19:54:58 +0530

    chore: setting up env variables

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
 
+
+const port = process.env.PORT || 3000;
+const token = process.env.TOKEN;
+
+
+
+
 app.get('/', (req, res) => {
+    console.log(port, token, baseUrl)
   res.send("I am a backend server")
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is running on port 3000")
 })
