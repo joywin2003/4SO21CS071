9d9e153b85364265cf00202774885949b3a1541a
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-07-18 20:21:54 +0530

    feat: added api request for all type

--- index.js
+++ index.js
@@ -9,26 +9,34 @@ const port = process.env.PORT || 3000;
 const token = process.env.TOKEN;
 const baseUrl = process.env.BASE_URL;
 
+const numberEndpoint = {
+  e: `even`,
+  p: `primes`,
+  f: `fibo`,
+  r: `rand`,
+};
+
 app.get("/", (req, res) => {
   console.log(port, token, baseUrl);
   res.send("I am a backend server");
 });
 
-app.get("/api", async (req, res) => {
-  try {
-    console.log(`${baseUrl}/even`);
-    const response = await axios(`${baseUrl}even`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log(response);
-
-    res.send(response.data.numbers);
-  } catch (error) {
-    console.log("error");
+app.get("/numbers/:numberId", async (req, res) => {
+  const numberId = req.params.numberId;
+  console.log(numberId);
+  if (!numberEndpoint[numberId]) {
+    res.send("Invalid numberId");
+    return;
   }
-  
+  try {
+    const response = await axios.get(`${baseUrl}${numberEndpoint[numberId]}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    res.send({ number_id: response.data });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send({ error: "Failed to fetch data" });
+  }
 });
 
 app.listen(port, () => {
